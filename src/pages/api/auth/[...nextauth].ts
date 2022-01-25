632c2efd606ef336511a662d080833468b7e395f
Curia <admin@curiadesign.net>
2022-01-26 10:14:52 +1100

    feat: added storybook and lint fixes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth";
-import StravaProvider from "next-auth/providers/strava";
+import NextAuth from 'next-auth';
+import StravaProvider from 'next-auth/providers/strava';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,20 +8,20 @@ export default NextAuth({
     StravaProvider({
       clientId: process.env.STRAVA_CLIENT_ID,
       clientSecret: process.env.STRAVA_CLIENT_SECRET,
-      authorization: { params: { scope: "read,activity:read_all" } },
+      authorization: { params: { scope: `read,activity:read_all` } },
     }),
   ],
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
-        token.accessToken = account.access_token
+        token.accessToken = account.access_token;
       }
-      return token
+      return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken
-      return session
+      session.accessToken = token.accessToken;
+      return session;
     },
   },
-})
+});
